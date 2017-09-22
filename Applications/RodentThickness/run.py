2c291a5f61cabc7ad28022bb9c8ae1ed4ad8cce0
Juan Carlos Prieto <juanprietob@gmail.com>
2017-09-22 14:33:10 -0400

    BUG: Reference RScript path using lower case letter as in the parameters

--- Applications/RodentThickness/run.py
+++ Applications/RodentThickness/run.py
@@ -289,7 +289,7 @@ def perform_analysis(opts, config, csvdata, outputDir, corr_opt="initial_dense")
         datafiles.append((group, datafilename))
 
     # perform statistical analysis
-    rscriptExePath = config["RScriptPath"]
+    rscriptExePath = config["RscriptPath"]
     rscriptPath = os.path.dirname(os.path.abspath(__file__)) + "/rodentThicknessStats.r"
     if (corr_opt == "initial_dense"):
         outputfile = "%s/stats.initialDenseSampling.txt" % statDir
